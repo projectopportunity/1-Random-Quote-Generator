ce99b5c1a9db37efad40bcd616e3e55d8df2ac62
tpo-cmaples <77011904+tpo-cmaples@users.noreply.github.com>
2021-01-11 13:56:24 -0500

    update formatting

--- js/script.js
+++ js/script.js
@@ -7,12 +7,42 @@ project 1 - A Random Quote Generator
  * Array of Objects containing quotes from various places.
 ***/
 const quotes = [
-  {quote: `Do, or do not. There is no try.`, source:`Master Yoda`, citation:`The Empire Strikes Back`, year: `1980`, type: `Movie`},
-  {quote: `A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.`, source:`Douglas Adams`, citation:`Mostly Harmless`, year: `1992`, type: `Book`},
-  {quote: `Dark times lie ahead of us and there will be a time when we must choose between what is easy and what is right.`, source:`Albus Dumbledore`, citation:`Harry Potter and the Sorcerer's Stone`, year: `2001`},
-  {quote: `Driver picks the music, shotgun shuts his cakehole.`, source:`Dean Winchester`, citation:`Supernatural`, type: `TV`},
-  {quote: `Time is a scary thing. When you lose it you can't find it. When you want it it's not there. When you try to keep it the hourglass turns itself around.`, source:`Nozdormu`, citation:`World of Warcraft`, type:`Video Game`},
-  {quote: `Whenever you are asked if you can do a job, tell 'em, 'Certainly I can!' Then get busy and find out how to do it.`, source:`Theodore Roosevelt`}
+  {
+    quote: `Do, or do not. There is no try.`, 
+    source:`Master Yoda`, 
+    citation:`The Empire Strikes Back`, 
+    year: `1980`, 
+    type: `Movie`
+  },
+  {
+    quote: `A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.`, 
+    source:`Douglas Adams`, 
+    citation:`Mostly Harmless`,
+    year: `1992`, 
+    type: `Book`
+  },
+  {
+    quote: `Dark times lie ahead of us and there will be a time when we must choose between what is easy and what is right.`, 
+    source:`Albus Dumbledore`, 
+    citation:`Harry Potter and the Sorcerer's Stone`, 
+    year: `2001`
+  },
+  {
+    quote: `Driver picks the music, shotgun shuts his cakehole.`, 
+    source:`Dean Winchester`, 
+    citation:`Supernatural`, 
+    type: `TV`
+  },
+  {
+    quote: `Time is a scary thing. When you lose it you can't find it. When you want it it's not there. When you try to keep it the hourglass turns itself around.`, 
+    source:`Nozdormu`, 
+    citation:`World of Warcraft`, 
+    type:`Video Game`
+  },
+  {
+    quote: `Whenever you are asked if you can do a job, tell 'em, 'Certainly I can!' Then get busy and find out how to do it.`, 
+    source:`Theodore Roosevelt`
+  }
 ];
 
 /***
@@ -21,13 +51,10 @@ const quotes = [
 function getRandomQuote() {
     
   //variable to store random number based on the number of objects in the quote array
-  let qNum = Math.floor( Math.random() * quotes.length);
+  const randomQuoteNumber = Math.floor( Math.random() * quotes.length);
   
   //return a random quotes Keys and Properties based on the previously generated number
-  return quotes[qNum];
-  
-  //combined into one line but does it meet the instructions?
-  //return quotes[Math.floor( Math.random() * quotes.length)]
+  return quotes[randomQuoteNumber];
 }
  
 /***
@@ -35,18 +62,20 @@ function getRandomQuote() {
 ***/
 function printQuote () {
   //Get a random quote
-  let qNum = getRandomQuote();
+  const randomQuote = getRandomQuote();
   
   //Declare html string to display quote
   let html = ``;
- 
-  //build html string for quote, source, and each additional Key/Property pair that exists
-    for (let obj in qNum) {  
-      if ( obj === `quote`) {
-        html += `<p class="quote">${qNum.quote}</p>`;
-      }else if ( obj === 'source' ) {
-        html += `<p class="source">${qNum.source}`; //closing p tag intentionally left off in order to add span class elements
-      }else {html += `<span class="${obj}">${qNum[obj]}</span>`};
+
+  //build html string for quote and source Key/Property
+  html += `<p class="quote">${randomQuote.quote}</p>`;
+  html += `<p class="source">${randomQuote.source}`; //closing p tag intentionally left off in order to add span class elements
+
+  //build html string for each additional Key/Property pair that exists
+    for (let prop in randomQuote) {  
+      if ( prop !== "quote" && prop !== "source" ) {
+        html += `<span class="${prop}">${randomQuote[prop]}</span>`
+      };
     };
 
   //add the closing p tag to span class
@@ -56,14 +85,17 @@ function printQuote () {
   document.getElementById('quote-box').innerHTML = html;
 
   //change the background to a random color
-  color = `${Math.floor( Math.random() *255)}, ${Math.floor( Math.random() *255)}, ${Math.floor( Math.random() *255)}`;
-  document.body.style.backgroundColor = `rgb(${color})`;
+  function getRandomRgbComponent(){
+    return Math.floor( Math.random() *200);
+  };
+  const rgbString = `${getRandomRgbComponent()}, ${getRandomRgbComponent()}, ${getRandomRgbComponent()}`;
+  document.body.style.backgroundColor = `rgb(${rgbString})`;
 }
 
 /***
  * Pull a new quote every 10 seconds by triggering the printQuote function
 ***/
-setInterval(function () { printQuote(); }, 10000);
+window.setInterval(printQuote, 10000);
 
 /***
  * Call the printQuote function when the load-quote button is pressed
